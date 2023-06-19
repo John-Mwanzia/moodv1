feacf028b2d41067cf9b6c09b2d159e66199fb4c
John mwanzia <johnmwanzia277@gmail.com>
2023-06-19 21:52:28 +0300

    automatic update the analysis in the editor

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -20,46 +20,13 @@ const getEntry = async id => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id);
-  const {mood, summary,subject,color, negative} = entry?.analysis;
-
-  const analysisData = [
-    {
-      name: 'Summary',
-      value: summary
-    },
-    {
-      name: 'Subject',
-      value: subject
-    },
-    {
-      name: 'Mood',
-      value: mood
-    },
-    {
-      name: 'Negative',
-      value: negative?'true':'false'
-    }
-  ];
+ 
   return (
-    <div className="h-full w-full grid grid-cols-3 overflow-x-hidden">
-      <div className="col-span-2">
+    <div className="h-full w-full overflow-x-hidden ">
+      <div>
         <Editor entry={entry} />
       </div>
-      <div className="border-l border-black/5">
-        <div className="px-6 py-10 "  style={{backgroundColor: color}}>
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
-        <div>
-            <ul>
-                {analysisData.map((data)=>(
-                    <li key={data.name} className="flex justify-between border-b border-t px-2 py-4 border-black/10">
-                        <span className='text-lg font-semibold'>{data.name}</span>
-                        <span>{data.value}</span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-      </div>
+   
     </div>
   );
 };
--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -4,12 +4,35 @@ import { useState } from "react"
 import Spinner from "./Spinner"
 import { useAutosave } from 'react-autosave'
 import { updateEntry } from "@/utils/api"
+import { set } from "zod"
 
 
 const Editor = ({entry}) => {
     const [value, setValue] = useState(entry.content)
   const [currentEntry, setEntry] = useState(entry)
   const [isloading,  setIsLoading] = useState(false)
+  const [analysis, setAnalysis] = useState(entry?.analysis)
+
+  const {mood, summary,subject,color, negative} = analysis;
+
+  const analysisData = [
+    {
+      name: 'Summary',
+      value: summary
+    },
+    {
+      name: 'Subject',
+      value: subject
+    },
+    {
+      name: 'Mood',
+      value: mood
+    },
+    {
+      name: 'Negative',
+      value: negative?'true':'false'
+    }
+  ];
 
   useAutosave({
     data: value,
@@ -18,14 +41,15 @@ const Editor = ({entry}) => {
       setIsLoading(true)
 
       const updated = await updateEntry(entry.id,  _value )
+      setAnalysis(updated.analysis)
 
-      setEntry(updated)
+      setEntry(updated.analysis)
       setIsLoading(false)
     },
   })
 
   return (
-    <div className="w-full h-full relative ">
+    <div className="w-full h-full relative grid grid-cols-3 ">
     <div className="absolute left-0 top-0 p-2">
       {isloading ? (
         <Spinner />
@@ -33,49 +57,28 @@ const Editor = ({entry}) => {
         <div className="w-[16px] h-[16px] rounded-full bg-green-500"></div>
       )}
     </div>
-    <div className="w-full h-full">
+    <div className="w-full h-full col-span-2">
       <textarea
         value={value}
         onChange={(e) => setValue(e.target.value)}
         className="w-full h-full text-xl p-8 outline-none"
       />
     </div>
-    {/* <div className="border-l border-black/5">
-      <div
-        style={{ background: currentEntry.analysis.color }}
-        className="h-[100px] bg-blue-600 text-white p-8"
-      >
-        <h2 className="text-2xl bg-white/25 text-black">Analysis</h2>
+    <div className="border-l border-black/5">
+        <div className="px-6 py-10 "  style={{backgroundColor: color}}>
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+            <ul>
+                {analysisData.map((data)=>(
+                    <li key={data.name} className="flex justify-between border-b border-t px-2 py-4 border-black/10">
+                        <span className='text-lg font-semibold'>{data.name}</span>
+                        <span>{data.value}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
       </div>
-      <div>
-        <ul role="list" className="divide-y divide-gray-200">
-          <li className="py-4 px-8 flex items-center justify-between">
-            <div className="text-xl font-semibold w-1/3">Subject</div>
-            <div className="text-xl">{currentEntry.analysis.subject}</div>
-          </li>
-
-          <li className="py-4 px-8 flex items-center justify-between">
-            <div className="text-xl font-semibold">Mood</div>
-            <div className="text-xl">{currentEntry.analysis.mood}</div>
-          </li>
-
-          <li className="py-4 px-8 flex items-center justify-between">
-            <div className="text-xl font-semibold">Negative</div>
-            <div className="text-xl">
-              {currentEntry.analysis.negative ? 'True' : 'False'}
-            </div>
-          </li>
-          <li className="py-4 px-8 flex items-center justify-between">
-            <button
-              type="button"
-              className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-            >
-              Delete
-            </button>
-          </li>
-        </ul>
-      </div>
-    </div> */}
   </div>
 )
 }
