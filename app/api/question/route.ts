badc66146260441441d48791791ab39eba0d6528
John mwanzia <johnmwanzia277@gmail.com>
2023-06-20 13:05:00 +0300

    update prisma schema

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -1,23 +1,24 @@
-import { qa } from "@/utils/ai"
-import { getUserByClerkId } from "@/utils/auth"
-import { prisma } from "@/utils/db"
-import { NextResponse } from "next/server"
+import { qa } from '@/utils/ai'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { NextResponse } from 'next/server'
 
-export const  POST = async (req) => {
-    const {question } = await req.json()
-    const user = await getUserByClerkId()
-    const entries = await prisma.journalEntry.findMany({
-        where : {
-            userId: user.id
-        },
-        select: {
-            id: true,
-            content: true,
-            createdAt: true
-        }
-    })
+export const POST = async (request) => {
+  const { question } = await request.json()
+  const user = await getUserByClerkId()
 
-    const answer = await qa(question, entries) 
-    
-    return NextResponse.json({data: answer})
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+    },
+  })
+
+  const answer = await qa(question, entries)
+
+  return NextResponse.json({ data: answer })
 }
