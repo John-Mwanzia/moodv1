49340b30082e35ada591fa89a18b7dbc1d9452a3
John mwanzia <johnmwanzia277@gmail.com>
2023-06-20 11:41:26 +0300

    manage loading as i send a quiz to askQuestion utility

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -2,6 +2,7 @@
 
 import { askQuestion } from '@/utils/api';
 import { useState } from 'react';
+import Spinner from './Spinner';
 
 const Question = () => {
   const [value, setValue] = useState('');
@@ -18,21 +19,32 @@ const Question = () => {
   };
 
   return (
-    <div>
+    <div className='relative'>
       <form onSubmit={handleSubmit}>
         <input
+          disabled={isloading}
           type="text"
           value={value}
           placeholder="Ask a question"
           onChange={e => setValue(e.target.value)}
           className="border border-black/20 px-4 py-2 text-lg rounded-tl-lg rounded-bl-lg"
         />
-        <button className="border border-black/20 bg-blue-500 px-4 py-2 rounded-tr-lg rounded-br-lg text-lg ">
+        <button
+          className="border border-black/20 bg-blue-500 px-4 py-2 rounded-tr-lg rounded-br-lg text-lg "
+          disabled={isloading}
+        >
           Ask
         </button>
       </form>
+
+      {isloading && (
+        <div className='absolute bottom-[-20px] left-4'>
+          <Spinner />
+        </div>
+      )}
+      {response && <div className="mt-4">{response}</div>}
     </div>
-  );
+  ); 
 };
 
 export default Question;
